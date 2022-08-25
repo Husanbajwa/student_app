94398a40eb374ff6fc7c8a29de6dc88941f510a2
Husanbajwa <76039437+Husanbajwa@users.noreply.github.com>
2022-08-25 23:08:33 +0530

    add a delete.html file to delete a room

--- appbase/views.py
+++ appbase/views.py
@@ -28,3 +28,20 @@ def createRoom(request):
     return render(request, 'appbase/room_form.html',context)
 
 
+def updateRoom(request, pk):
+    room = Room.objects.get(id = pk)
+    form=RoomForm(instance=room)
+    if request.method == 'POST':
+        form=RoomForm(request.POST,instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context = {'form':form}
+    return render(request, 'appbase/room_form.html',context)
+
+def deleteRoom(request,pk):
+    room = Room.objects.get(id = pk)
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+    return render(request, 'appbase/delete.html' ,{'obj':room})

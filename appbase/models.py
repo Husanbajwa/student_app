d74759cad3a1945544e383b1066c9c7f6675b9e3
Husanbajwa <76039437+Husanbajwa@users.noreply.github.com>
2022-08-25 23:48:24 +0530

    create a side panel to filter rooms according to topics

--- appbase/models.py
+++ appbase/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
 class Topic(models.Model):
     name=models.CharField(max_length=200)
     def __str__(self):
--- appbase/views.py
+++ appbase/views.py
@@ -1,14 +1,12 @@
 from django.shortcuts import render , redirect
-from .models import Room
+from .models import Room, Topic
 from .forms import RoomForm
-
-
 # Create your views here.
-
-
 def home(request):
-    rooms= Room.objects.all()
-    context = {'rooms': rooms}
+    q=request.GET.get('q') if request.GET.get('q') != None else ''
+    rooms= Room.objects.filter(topic__name__icontains=q)
+    topics =Topic.objects.all()
+    context = {'rooms': rooms, 'topics': topics}
     return render(request, 'appbase/home.html', context)
 
 

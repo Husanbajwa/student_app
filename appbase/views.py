5a0c40bda493e4de104e49ce721beda57bf8ec22
Husanbajwa <76039437+Husanbajwa@users.noreply.github.com>
2022-08-25 21:01:57 +0530

    created a form to save and create a new room and the data will be save to database

--- appbase/views.py
+++ appbase/views.py
@@ -1,7 +1,6 @@
-from multiprocessing import context
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 from .models import Room
-
+from .forms import RoomForm
 
 
 # Create your views here.
@@ -19,5 +18,11 @@ def room(request, pk):
     return render(request, 'appbase/room.html',context)
 
 def createRoom(request):
-    context={}
+    form=RoomForm()
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context={'form':form}
     return render(request, 'appbase/room_form.html',context)

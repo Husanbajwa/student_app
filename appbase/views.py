3aeda290fe7736691d890ec76d03f7911e4157b2
Husanbajwa <76039437+Husanbajwa@users.noreply.github.com>
2022-09-04 20:05:39 +0530

    create a recent activity page

--- appbase/views.py
+++ appbase/views.py
@@ -65,7 +65,8 @@ def home(request):
         )
     topics =Topic.objects.all()
     room_count = rooms.count()
-    context = {'rooms': rooms, 'topics': topics,'room_count':room_count}
+    room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
+    context = {'rooms': rooms, 'topics': topics,'room_count':room_count,'room_messages':room_messages}
     return render(request, 'appbase/home.html', context)
 
 

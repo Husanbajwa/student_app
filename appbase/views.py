84e674bfe7c4595160bc4ff64087b33ad7ebc3bc
Husanbajwa <76039437+Husanbajwa@users.noreply.github.com>
2022-10-06 19:39:27 +0530

    add css and create themes

--- appbase/views.py
+++ appbase/views.py
@@ -1,3 +1,4 @@
+from pydoc_data.topics import topics
 from django.shortcuts import render , redirect
 from django.http import HttpResponse
 from django.contrib import messages
@@ -98,14 +99,19 @@ def userProfile(request,pk):
 @login_required(login_url='login')
 def createRoom(request):
     form=RoomForm()
+    topics =Topic.objects.all()
     if request.method == 'POST':
-        form = RoomForm(request.POST)
-        if form.is_valid():
-            room = form.save(commit=False)
-            room.host = request.user
-            room.save()
-            return redirect('home')
-    context={'form':form}
+        topic_name = request.POST.get('topic')
+        topic,created = Topic.objects.get_or_create(name=topic_name)
+        
+        Room.objects.create(
+            host=request.user,
+            topic=topic,
+            name=request.POST.get('name'),
+            description=request.POST.get('description'),
+        )
+        return redirect('home')
+    context={'form':form, 'topics':topics}
     return render(request, 'appbase/room_form.html',context)
 
 
@@ -113,16 +119,19 @@ def createRoom(request):
 def updateRoom(request, pk):
     room = Room.objects.get(id = pk)
     form=RoomForm(instance=room)
-
+    topics =Topic.objects.all()
     if (request.user != room.host):
         return HttpResponse('You are not allowed here')
 
     if request.method == 'POST':
-        form=RoomForm(request.POST,instance=room)
-        if form.is_valid():
-            form.save()
-            return redirect('home')
-    context = {'form':form}
+        topic_name = request.POST.get('topic')
+        topic,created = Topic.objects.get_or_create(name=topic_name)
+        room.name = request.POST.get('name')
+        room.topic = topic
+        room.description = request.POST.get('description')
+        room.save()
+        return redirect('home')
+    context = {'form':form,'topics':topics,'room':room}
     return render(request, 'appbase/room_form.html',context)
 
 
--- static/js/nav-script.js
+++ static/js/nav-script.js
@@ -7,7 +7,7 @@ const profileBtn = document.querySelector(".profile");
 const dropUpBtn = document.querySelector(".dropupbtn");
 const dropDownBtn = document.querySelector(".dropdownbtn");
 const dropDownItem = document.querySelector(".dropdown");
-
+const contenShift = document.querySelector(".content");
 menuBtn.onclick = () => {
     menuitems.classList.add("active");
     menuBtn.classList.add("hide");
@@ -22,20 +22,35 @@ cancelBtn.onclick = () => {
     cancelBtn.classList.remove("show");
     form.classList.remove("active");
     dropDownItem.classList.remove("active");
+    dropDownBtn.classList.remove("hide");
+    dropUpBtn.classList.remove("show");
+    contenShift.classList.remove("active");
 }
 searchBtn.onclick = () => {
+    contenShift.classList.add("active");
     form.classList.add("active");
     searchBtn.classList.add("hide");
     cancelBtn.classList.add("show");
     dropDownItem.classList.remove("active");
-}
-dropUpBtn.onclick =() => {
-    dropDownItem.classList.remove("active");
     dropDownBtn.classList.remove("hide");
     dropUpBtn.classList.remove("show");
+    
 }
 dropDownBtn.onclick = () => {
     dropDownItem.classList.add("active");
     dropDownBtn.classList.add("hide");
     dropUpBtn.classList.add("show");
+    menuitems.classList.remove("active");
+    cancelBtn.classList.remove("show");
+    menuBtn.classList.remove("hide");
+    searchBtn.classList.remove("hide");
+    contenShift.classList.remove("active");
+    form.classList.remove("active");
+}
+dropUpBtn.onclick =() => {
+    dropDownItem.classList.remove("active");
+    dropDownBtn.classList.remove("hide");
+    dropUpBtn.classList.remove("show");
+    menuBtn.classList.remove("hide");
+    searchBtn.classList.remove("hide");
 }
